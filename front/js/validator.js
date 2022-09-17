48dbc18923258fcb8cd6a49651e8f4ce3b8cccea
maksgir <maksgirya@mail.ru>
2022-09-17 21:11:22 +0300

    js integration + form data validation

--- front/js/validator.js
+++ front/js/validator.js
@@ -1,18 +1,22 @@
-function isCorrect() {
+function isCorrect(x, y, r) {
 
-    alert("иди нахуй");
+    console.log("пришли " + x + " " + y + " " + r)
 
-    // let x_is_correct = [-4, -3, -2, -1, 0, 1, 2, 3, 4].includes(x);
-    //
-    // let y_is_correct = false;
-    //
-    // if (!isNaN(y)) {
-    //     y_is_correct = y <= 3 && y >= -3;
-    // }
-    //
-    // let r_is_correct = [1, 1.5, 2, 2.5, 3].includes(r);
-    //
-    //
-    //
-    // return x_is_correct && y_is_correct && r_is_correct;
+
+    let x_is_correct = [-4, -3, -2, -1, 0, 1, 2, 3, 4].includes(Number(x));
+
+    let y_is_correct = false;
+
+    if (!(y === undefined || y === "")) {
+        y_is_correct = y <= 3 && y >= -3;
+    }
+
+    let r_is_correct = [1, 1.5, 2, 2.5, 3].includes(Number(r));
+
+    console.log("значение х - " + x +" валидно: " + x_is_correct);
+    console.log("значение y - " + y +" валидно: " + y_is_correct);
+    console.log("значение r - " + r +" валидно: " + r_is_correct);
+
+
+    return x_is_correct && y_is_correct && r_is_correct;
 }
